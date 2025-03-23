139d03a29a3eca74acba990ae89c5d1db29c8eb0
pay501 <junoir151040@gmail.com>
2025-03-23 21:54:30 +0700

    modify sidebar

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,46 +1,83 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { User } from "../utils/types";
+import logo from "../assets/northern bank logo.png";
 
 type SidebarProps = {
   userData: User | null;
 };
 
+const sideBarLink = [
+  {
+    "name":"หน้าหลัก",
+    "path":"/"
+  },  
+  {
+    "name":"สินเชื่อ",
+    "path":"/loan"
+  },
+  {
+    "name":"โอนเงิน",
+    "path":"/fundtransfer"
+  },
+  {
+    "name":"ประวัติรายการ",
+    "path":"/transfer-history"
+  },
+  {
+    "name":"ตั้งค่าบัญชี",
+    "path":"/settings"
+  },
+  {
+    "name":"ช่วยเหลือ",
+    "path":"/help"
+  },
+]
+
 const Sidebar: React.FC<SidebarProps> = ({ userData }) => {
+  
+  const pathName = window.location.pathname;
+
   return (
-    <aside className="w-64 bg-green-600 text-white h-full">
+    <aside className="bg-green-600 text-white h-full">
       <div className="p-4 text-center border-b border-green-600">
-        <h1 className="text-xl font-bold">NORTHERN BANK</h1>
+        <h1 className="text-2xl font-bold pt-4">NORTHERN BANK</h1>
       </div>
       <div className="p-4 text-center">
-        <div className="h-20 w-20 mx-auto rounded-full bg-gray-400"></div>
-        <h2 className="mt-4 text-lg">{userData?.first_name} {userData?.last_name}</h2>
+        <img
+          src={logo}
+          alt=""
+          width={96}
+          height={96}
+          className="rounded-full mx-auto"
+        />
+        <h2 className="mt-4 text-lg">
+          {userData?.first_name} {userData?.last_name}
+        </h2>
         <p className="text-sm">บัญชีธุรกิจ</p>
       </div>
-      <nav>
-        <ul className="overflow-y-auto">
-          <li className="px-4 py-2 hover:bg-green-700">
-            <Link className="w-64" to="/">โฮม</Link>
-          </li>
-          <li className="px-4 py-2 hover:bg-green-700">
-            <Link to="/loan">สินเชื่อ</Link>
-          </li>
-          <li className="px-4 py-2 hover:bg-green-700">
-            <Link to="/fundtransfer">โอนเงิน</Link>
-          </li>
-          <li className="px-4 py-2 hover:bg-green-700">
-            <Link to="/history">ประวัติรายการ</Link>
-          </li>
-          <li className="px-4 py-2 hover:bg-green-700">
-            <Link to="/settings">ตั้งค่าบัญชี</Link>
-          </li>
-          <li className="px-4 py-2 hover:bg-green-700">
-            <Link to="/help">ช่วยเหลือ</Link>
-          </li>
-        </ul>
-      </nav>
+      {
+        sideBarLink.map((val: any, index: number)=>{
+          return(
+            <Link 
+              to={val.path} 
+              className={
+                pathName === val.path ? 
+                `flex bg-green-700 pl-2 py-2 text-lg font-semibold`:
+                `flex bg-green-600 pl-2 py-2 text-lg font-semibold hover:bg-green-700 transition-colors duration-500 ease-in-out`
+              }
+              key={index}
+            >
+              {val.name}
+            </Link>
+          ) 
+        })
+      }
     </aside>
   );
 };
 
 export default Sidebar;
+
+
+

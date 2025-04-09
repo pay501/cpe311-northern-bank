d3affb9a97cbe95e9e8d818bca0f7eeec3197916
pay501 <junoir151040@gmail.com>
2025-04-09 18:33:29 +0700

    Modify: Add feature logout

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { User } from "../utils/types";
+import { useNavigate } from "react-router-dom";
 
 type HeaderProps = {
   userData: User | null;
 };
 
 const Header: React.FC<HeaderProps> = ({ userData }) => {
+  
+  const navigate = useNavigate()
+  
   return (
     <header className="bg-white p-4 shadow-md flex justify-between items-center">
       {
@@ -18,7 +22,16 @@ const Header: React.FC<HeaderProps> = ({ userData }) => {
       <div className="flex items-center space-x-4">
         <span>{userData?.first_name} {userData?.last_name}</span>
         <button className="text-green-900">ไทย / English</button>
-        <div className="h-8 w-8 bg-gray-300 rounded-full"></div>
+        <button 
+          className="bg-red-500 opacity-80 px-2 py-1 rounded-lg text-white font-mono
+           hover:bg-red-500 hover:opacity-100"
+          onClick={()=>{
+            sessionStorage.removeItem("jwtToken");
+            navigate("/authen")
+          }}
+        >
+          logout
+        </button>
       </div>
     </header>
   );

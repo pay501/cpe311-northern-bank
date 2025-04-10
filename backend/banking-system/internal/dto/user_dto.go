debd6289ab9b4ea342348b2419bcba65e01ead7e
pay501 <junoir151040@gmail.com>
2025-04-10 21:14:49 +0700

    Modify: Create struct

--- backend/banking-system/internal/dto/user_dto.go
+++ backend/banking-system/internal/dto/user_dto.go
@@ -4,3 +4,18 @@ type UpdateUserInformation struct {
 	PhoneNumber *string `json:"phone_number" validate:"omitempty,len=10,numeric"`
 	Email       *string `json:"email" validate:"omitempty,email"`
 }
+
+type CheckRecieverAccountReq struct {
+	AccountNumber string `json:"account_number" validate:"required,len=10,numeric"`
+	BankCode      string `json:"bank_code" validate:"required"`
+}
+
+type AccountWithOwner struct {
+	AccID     string
+	AccNo     string
+	BankCode  string
+	Balance   float64
+	UserID    int
+	FirstName string
+	LastName  string
+}
